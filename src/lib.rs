d2b05f06197411f480c3dd5dac7fe00f953ee3df
Mukesh Ranjan <mukeshranjan@disroot.org>
2025-12-31 05:39:54 +0000

    committing latest

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[program]
+pub mod profiles{
+    use super::*;
+    
+    pub fn create_profile(
+        ctx: Context,
+        name: String,
+        headline: String,
+        bio: String,
+        headline: String,
+        bio: String,
+        ) -> Result<()>{
+
+
+    }   
+}
